45feb1b3842e4fad6c822e94850597da930aa18d
Pavlo Hrab <pavlo.hrab@gmail.com>
2022-02-17 12:27:53 +0100

    Added test to pass for the script

--- first_file.py
+++ first_file.py
@@ -5,5 +5,9 @@ import numpy
 def multiply_numbers_in_list(list_numbers):
   result = 1.
   for num_i in list_numbers:
-    result= result**num_i
+    result= result*num_i
   return result
+
+def test_multiply_numbers_in_list():
+    assert multiply_numbers_in_list([1,2,3,4]) == 24
+    assert multiply_numbers_in_list([0,3,4]) == 0
