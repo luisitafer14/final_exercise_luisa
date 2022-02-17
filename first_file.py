37e028a3cfdc7687c5398c8c6dcd5dfe778fa90a
luisitafer14 <luisitafer_14@hotmail.com>
2022-02-17 12:18:02 +0100

    Adding a function with a bug!

--- first_file.py
+++ first_file.py
@@ -1,2 +1,9 @@
 #This is the first file
 import numpy
+
+#This is a function containing a bug to fix
+def multiply_numbers_in_list(list_numbers):
+  result = 1.
+  for num_i in list_numbers:
+    result= result**num_i
+  return result
